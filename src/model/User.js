6e72bd0dad2b3197118369722ae6491386866b52
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-12-05 12:11:35 +0700

    set the default = 0

--- src/model/User.js
+++ src/model/User.js
@@ -4,24 +4,65 @@ const userSchema = new mongoose.Schema({
     name:String,
     email:String,
     password:String,
+    
     age:Number,
     gender:String,
     height:Number,
     weight:Number,
     activity:String,
-    BMR:Number,
-    TDEE:Number,
-    carbsMax:Number,
-    carbsMin:Number,
-    fatMax:Number,
-    fatMin:Number,
-    proteinMax:Number,
-    proteinMin:Number,
-    saltMax:Number,
-    saltMin:Number,
-    sugarMax:Number,
-    sugarMin:Number,
-    waterRequirement:Number,
+
+    BMR:{
+        type:Number,
+        default:0
+    },
+    TDEE:{
+        type:Number,
+        default:0
+    },
+    carbsMax:{
+        type:Number,
+        default:0
+    },
+    carbsMin:{
+        type:Number,
+        default:0
+    },
+    fatMax:{
+        type:Number,
+        default:0
+    },
+    fatMin:{
+        type:Number,
+        default:0
+    },
+    proteinMax:{
+        type:Number,
+        default:0
+    },
+    proteinMin:{
+        type:Number,
+        default:0
+    },
+    saltMax:{
+        type:Number,
+        default:0
+    },
+    saltMin:{
+        type:Number,
+        default:0
+    },
+    sugarMax:{
+        type:Number,
+        default:0
+    },
+    sugarMin:{
+        type:Number,
+        default:0
+    },
+    waterRequirement:{
+        type:Number,
+        default:0
+    },
 
 })
 
